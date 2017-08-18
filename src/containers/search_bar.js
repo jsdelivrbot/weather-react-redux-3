4c18dcc955e0c0bb012a06c01a9ae0a097f8f604
James Attard <james@Apples-MacBook-Air.local>
2017-08-18 18:41:50 +0200

    Form Elements

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -12,13 +12,16 @@ export default class SearchBar extends Component {
   }
 
   onInputChange(e) {
-    console.log(e.target.value);
     this.setState({ term: e.target.value });
+  }
+
+  onFormSubmit(e) {
+    e.preventDefault();
   }
 
   render() {
     return (
-      <form className="input-group">
+      <form onSubmit={this.onFormSubmit} className="input-group">
         <input
           // placeholder="Get a five-day forecast in your favorite cities"
           placeholder="Forecast"
