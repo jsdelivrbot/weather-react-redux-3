de4153879b5cc7cad5a79e359146622c7f2107e2
James Attard <james@Apples-MacBook-Air.local>
2017-08-20 16:37:45 +0200

    Avoid state manipulation

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,14 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+  
   console.log('Action received', action);
+  
+  switch(action.type) {
+    case FETCH_WEATHER:
+      // return state.concat([ action.payload.data ]);
+      return [ action.payload.data, ...state ]; // equivalent to [ city, city, city ] NOT [ city, [ city, city ]]
+  }
+
   return state;
 }
