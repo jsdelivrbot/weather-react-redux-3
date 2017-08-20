d2d47f43dbb5f6e44f004f550936f52b63bac67f
James Attard <james@Apples-MacBook-Air.local>
2017-08-20 15:51:07 +0200

    Console log action

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 export default function(state = null, action) {
   console.log('Action received', action);
-=======
-export default function(state = null, action){
->>>>>>> ffbee3ee8ce8350dde602eae2cdcca1efff65449
   return state;
 }
